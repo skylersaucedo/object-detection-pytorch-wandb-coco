0eb2775ae0990ffd0d819a0bdf92cfb9120734b1
skylersaucedo <skyler.saucedo@gmail.com>
2023-05-03 06:32:41 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+ROOT_PATH = 'input/Dataset 1 (Simplex)/Dataset 1 (Simplex)'
+TEST_PATH = 'input/Dataset 1 (Simplex)/Dataset 1 (Simplex)/Test data'
+PREDICTION_THRES = 0.8
+EPOCHS = 5
+MIN_SIZE = 800
+BATCH_SIZE = 2
+DEBUG = False # to visualize the images before training
